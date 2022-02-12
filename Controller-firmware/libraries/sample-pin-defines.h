ff403a7ee548e4230ac92f6988af980b175107d0
rhinocerose <ashar.k.latif@gmail.com>
2022-02-12 17:23:36 -0500

    Added libraries for touchscreen and light sensor

---
+++ Controller-firmware/libraries/sample-pin-defines.h
@@ -0,0 +1,11 @@
+// Include pin definitions in 'main.h' like this
+
+// For the display
+#define LCD_CS_Pin 			GPIO_PIN_6
+#define LCD_CS_GPIO_Port 	GPIOB
+#define LCD_DC_Pin 			GPIO_PIN_5
+#define LCD_DC_GPIO_Port 	GPIOC
+#define LCD_RST_Pin 		GPIO_PIN_4
+#define LCD_RST_GPIO_Port 	GPIOC
+
+
